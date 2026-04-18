67518179d990b6a4c005a8dc40f45ef59311446e
Tikva321 <81hdjdbdjhjd@gmail.com>
2026-04-18 15:26:25 +0300

    main.py

--- main.py
+++ main.py
@@ -34,13 +34,34 @@ class Player(GameSprite):
         if keys[K_s] and self.rect.y < win_height - 110: 
             self.rect.y += self.speed
 
+class Ball(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, image_width, image_height):
+        super().__init__(player_image, player_x, player_y, player_speed, image_width, image_height)
+        self.direction_x = 1  
+        self.direction_y = 1  
+        
+    def update(self):
+        self.rect.x += self.speed * self.direction_x
+        self.rect.y += self.speed * self.direction_y
+        
+        if self.rect.y <= 0 or self.rect.y >= win_height - 60:
+            self.direction_y *= -1
+        
+        if self.rect.colliderect(player1.rect) and self.direction_x == -1:
+            self.direction_x *= -1
+            self.direction_y = 1 if self.rect.centery > player1.rect.centery else -1
+        
+        if self.rect.colliderect(player2.rect) and self.direction_x == 1:
+            self.direction_x *= -1
+            self.direction_y = 1 if self.rect.centery > player2.rect.centery else -1
+
 clock = time.Clock()
 FPS = 60
 game = True
 
 player1 = Player('racket.png', 50, 200, 5, 20, 100)  
 player2 = Player('racket.png', 535, 200, 5, 20, 100)  
-player3 = Player('ball.png', 270, 220, 5, 60, 60)  
+ball = Ball('ball.png', 270, 220, 3, 60, 60)  
 
 while game:
     for e in event.get():
@@ -49,11 +70,12 @@ while game:
 
     player1.update_1()
     player2.update_2() 
+    ball.update()
 
     window.fill(back)
     player1.reset()
     player2.reset()
-    player3.reset()
+    ball.reset()
 
     display.update()
     clock.tick(FPS)
