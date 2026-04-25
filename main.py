aa254c8c73ab05f5f63271494c444aad91a43d54
Tikva321 <81hdjdbdjhjd@gmail.com>
2026-04-25 14:41:56 +0300

    main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 
+font.init()
+font_score = font.Font(None, 36)
+font_message = font.Font(None, 72)
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, image_width, image_height):
         super().__init__()
@@ -55,27 +59,65 @@ class Ball(GameSprite):
             self.direction_x *= -1
             self.direction_y = 1 if self.rect.centery > player2.rect.centery else -1
 
+    def reset_ball(self):
+        ball.rect.x = win_width // 2 - 25
+        ball.rect.y = win_height // 2 - 25
+        ball.direction_x = 1 if ball.direction_x == -1 else -1
+        ball.direction_y = 1
+
 clock = time.Clock()
 FPS = 60
 game = True
+game_over = False
+score_1 = 0
+score_2 = 0
+win_score = 3
 
 player1 = Player('racket.png', 50, 200, 5, 20, 100)  
 player2 = Player('racket.png', 535, 200, 5, 20, 100)  
-ball = Ball('ball.png', 270, 220, 3, 60, 60)  
+ball = Ball('ball.png', 270, 220, 2, 60, 60)  
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
 
-    player1.update_1()
-    player2.update_2() 
-    ball.update()
+    if not game_over:
+        player1.update_1()
+        player2.update_2() 
+        ball.update()
+        
+        if ball.rect.x <= 0:
+            score_2 += 1 
+            ball.reset_ball()
+            
+        if ball.rect.x >= win_width - 50:
+            score_1 += 1 
+            ball.reset_ball()
+        
+        if score_1 >= win_score:
+            game_over = True
+            winner = "Левый игрок победил!"
+        elif score_2 >= win_score:
+            game_over = True
+            winner = "Правый игрок победил!"
 
     window.fill(back)
     player1.reset()
     player2.reset()
     ball.reset()
+
+    score_1_text = font_score.render(str(score_1), True, (0, 0, 0))
+    score_2_text = font_score.render(str(score_2), True, (0, 0, 0))
+    window.blit(score_1_text, (win_width // 2 - 40, 20))
+    window.blit(score_2_text, (win_width // 2 + 20, 20))
+
+    if game_over:
+        win_text = font_message.render(winner, True, (255, 0, 0))
+        window.blit(win_text, (win_width // 2 - win_text.get_width() // 2, win_height // 2 - 50))
+
+    display.update()
+    clock.tick(FPS)
 
     display.update()
     clock.tick(FPS)
